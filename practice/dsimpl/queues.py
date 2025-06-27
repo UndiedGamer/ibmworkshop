b5b9afb9664c1b9c11cfbff56518436b874aae4f
UndiedGamer <hemanthchakka2007@gmail.com>
2025-06-27 10:42:15 +0530

    chore: my face when

--- practice/dsimpl/queues.py
+++ practice/dsimpl/queues.py
@@ -1,21 +1,21 @@
 class Queue():
     def __init__(self):
         self.items = []
+        self.front = -1
         self.rear = -1
-        self.back = -1
 
     def push(self, element):
         self.items.append(element)
-        if self.rear == -1:
-            self.rear = self.rear+1
-        self.back = self.back+1
+        if self.front == -1:
+            self.front = self.front+1
+        self.rear = self.rear+1
         print(f"Pushed {element} to stack")
 
     def pop(self):
-        if self.rear>0:
-            return self.items.pop(self.rear)
+        if self.front>0:
+            return self.items.pop(self.front)
         else:
             raise Exception("Queue Underflow")
         
     def peek(self):
-        return self.items[self.rear] if self.rear>0 else None
+        return self.items[self.front] if self.front>0 else None
