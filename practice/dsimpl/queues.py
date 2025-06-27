e7818bc7bff2d983ab315045b1b850f4764a6cd2
UndiedGamer <hemanthchakka2007@gmail.com>
2025-06-27 10:10:09 +0530

    chore: more practice, i guess

---
+++ practice/dsimpl/queues.py
@@ -0,0 +1,21 @@
+class Queue():
+    def __init__(self, items: list[any]):
+        self.items = items
+        self.rear = -1
+        self.back = -1
+
+    def push(self, element):
+        self.items.append(element)
+        if self.rear == -1:
+            self.rear = self.rear+1
+        self.back = self.back+1
+        print(f"Pushed {element} to stack")
+
+    def pop(self):
+        if self.rear>0:
+            return self.items.pop(self.rear)
+        else:
+            raise Exception("Queue Underflow")
+        
+    def peek(self):
+        return self.items[self.rear] if self.rear>0 else None
---
+++ practice/dsimpl/stacks.py
@@ -0,0 +1,19 @@
+class Stack():
+    def __init__(self, items: list[any]):
+        self.items = items
+        self.top = 0
+
+    def push(self, element):
+        self.items.append(element)
+        self.top = self.top+1
+        print(f"Pushed {element} to stack")
+
+    def pop(self):
+        if self.top>0:
+            self.top = self.top-1
+            return self.items.pop()
+        else:
+            raise Exception("Stack Underflow")
+        
+    def peek(self):
+        return self.items[self.top] if self.top>0 else None
